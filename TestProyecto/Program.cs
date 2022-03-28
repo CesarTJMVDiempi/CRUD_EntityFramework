a00a83bf11be75cb7d07e4c59861c5cdf4f03dda
Diempi33 <Diempi33@PC33>
2022-03-28 16:45:48 +0200

    Agregar archivos de proyecto.

---
+++ TestProyecto/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Inserte un número");
+string? ts = Console.In.ReadLine();
+int n;
+;
+
+
+if (ts == string.Empty) Console.WriteLine("No has escrito nada");
+else if (int.TryParse(ts, out n)) {
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine(n);
+} else {
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine(ts + " NO ES UN NÚMERO!");
+}
